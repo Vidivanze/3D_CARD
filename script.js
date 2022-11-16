170ab3eb4cafe4b61bc6c3041e49f5fa01f9a090
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-11-16 12:20:11 +0100

    Add rotation animation

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function init() {
 
   let glftLoader = new THREE.GLTFLoader();
   glftLoader.load('./assets/MagicPass_card.gltf', (gltf) => {
-
+    
     gltf.scene.rotation.z = Math.PI / 24;
     gltf.scene.rotation.y = Math.PI / 12;
     gltf.scene.position.y = 0.2;
@@ -27,7 +27,7 @@ function init() {
 
     sceneInstance.scene.add(loadedModel);
   });
-
+   
   document.addEventListener( 'mousemove', onMouseMove, false );
 
   animate();
@@ -38,48 +38,12 @@ function onMouseMove(event) {
 	mouse.y = ( event.clientY - windowHalf.y );
 }
 
-  // LIGHTS HELPERS
-  
-  const helper1 = new THREE.DirectionalLightHelper( frontLight1, 5 );
-  sceneInstance.scene.add( helper1 );
-  const helper2 = new THREE.DirectionalLightHelper( frontLight2, 5 );
-  sceneInstance.scene.add( helper2 );
 
-  const helper3 = new THREE.DirectionalLightHelper( backLight1, 5 );
-  scene.add( helper3 );
-  const helper4 = new THREE.DirectionalLightHelper( backLight2, 5 );
-  scene.add( helper4 );
-  */
- 
-  // MODEL LOADER
-  /*
-    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
-    loader.setDRACOLoader( dracoLoader );
-    */
+function animate() {
+  if(loadedModel) {
+    loadedModel.rotation.y = THREE.MathUtils.lerp(loadedModel.rotation.y, (mouse.x * Math.PI) / 10000, 0.1)
+    loadedModel.rotation.x = THREE.MathUtils.lerp(loadedModel.rotation.x, (mouse.y * Math.PI) / 9000, 0.1)
+  }
 
-    let loadedModel;
-    const glftLoader = new THREE.GLTFLoader();
-    glftLoader.load('./assets/MagicPass_card.gltf', (gltfScene) => {
-      loadedModel = gltfScene;
-      
-      gltfScene.scene.rotation.z = Math.PI / 10;
-      gltfScene.scene.rotation.y = Math.PI / 5;
-      gltfScene.scene.position.y = 0.5;
-      gltfScene.scene.scale.setScalar(1);
-
-      sceneInstance.scene.add(gltfScene.scene);
-    });
-
-    const animate = () => {
-     /*  if (loadedModel) {
-        loadedModel.scene.rotation.x += 0.01;
-        loadedModel.scene.rotation.y += 0.01;
-        loadedModel.scene.rotation.z += 0.01;
-      } */
-      requestAnimationFrame(animate);
-    };
-
-  animate();
+  requestAnimationFrame( animate );
 }
